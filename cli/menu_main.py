359be880995d2e30711560921c231ce8ccc04af8
jaredV54 <jaredV54.com>
2025-11-22 22:05:09 +0800

    Error import cli menus fixed

--- cli/menu_main.py
+++ cli/menu_main.py
@@ -1,5 +1,10 @@
 from utils.helpers import select_from_list
 from auth.register_user import register_user
+from cli.menu_drivers import driver_menu
+from cli.menu_packages import package_menu
+from cli.menu_routes import route_menu
+from cli.menu_tracking import tracking_menu
+from cli.menu_reports import reports_menu
 
 def menu_main(user_id, user_role, user_full_name, user_username):
     while True:
@@ -9,14 +14,22 @@ def menu_main(user_id, user_role, user_full_name, user_username):
         if user_role in ["admin", "dispatcher"]:
             menu_options.append("Package Management")
             menu_options.append("Route Management")
+            call_menu_functions.append(package_menu)
+            call_menu_functions.append(route_menu)
         if user_role in ["admin", "dispatcher", "driver"]:
             menu_options.append("Delivery Tracking")
+            call_menu_functions.append(tracking_menu)
         if user_role in ["admin", "dispatcher", "manager"]:
             menu_options.append("Reports")
+            call_menu_functions.append(reports_menu)
+        if user_role == "driver":
+            menu_options.append("Driver Dashboard")
+            call_menu_functions.append(driver_menu)
         if user_role == "admin":
+            call_menu_functions.append(register_user)
             menu_options.append("Register New User")
 
-        choice_input = select_from_list(menu_options, "Logging out...")
+        choice_input = select_from_list(menu_options, "Logging out...", "Log out", "====== MAIN MENU ======")
                 
         if choice_input is None or choice_input == 0:
             break
@@ -24,4 +37,5 @@ def menu_main(user_id, user_role, user_full_name, user_username):
         for options in menu_options:
             if menu_options.index(options) + 1 == choice_input:
                 print(f"You selected: {options}")
+                call_menu_functions[menu_options.index(options)]()
             
